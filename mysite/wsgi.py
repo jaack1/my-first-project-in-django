01adb3138ae989d0a825af6d094043c6e5968f6c
jaack1 <jack.90@mail.ru>
2017-07-03 21:04:11 +0300

    Changed the HTML for the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/jack/my-first-blog'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
